db0a016518eb3e8597a02c0a2f593e83d9cb057b
KenjiiiW <kenjiiiw@gmail.com>
2021-02-27 21:35:48 -0300

    Insercion de los contadores en informe

---
+++ src/views/Informe.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title style="background-color: #eeeeee" class="justify-center"><strong>INFORMES</strong></v-card-title>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title style="background-color:#90CAF9"><strong>Tesis de posgrado</strong><v-spacer></v-spacer>Totales: 5</v-card-title>
+                    <v-card-text></v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn dark color="primary">Detalles</v-btn>
+                        <v-btn dark color="primary">Exportar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    <v-card-title style="background-color:#90CAF9"><strong>Tesis de licenciatura</strong><v-spacer></v-spacer>Totales: 22</v-card-title>
+                    <v-card-text></v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn dark color="primary">Detalles</v-btn>
+                        <v-btn dark color="primary">Exportar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    <v-card-title style="background-color:#90CAF9"><strong>Prácticas profesionalizantes</strong><v-spacer></v-spacer>Totales: 3</v-card-title>
+                    <v-card-text></v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn dark color="primary">Detalles</v-btn>
+                        <v-btn dark color="primary">Exportar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title style="background-color:#90CAF9"><strong>Prácticas supervisadas</strong><v-spacer></v-spacer>Totales: 12</v-card-title>
+                    <v-card-text></v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn dark color="primary">Detalles</v-btn>
+                        <v-btn dark color="primary">Exportar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col>
+                <v-card>
+                    <v-card-title style="background-color:#90CAF9"><strong>Ensayos de cátedra</strong><v-spacer></v-spacer>Totales: 8</v-card-title>
+                    <v-card-text></v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn dark color="primary">Detalles</v-btn>
+                        <v-btn dark color="primary">Exportar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col>
+
+            </v-col>
+        </v-row>       
+    </v-container>
+</template>
